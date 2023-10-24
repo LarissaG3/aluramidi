5ceabe50198f4891fbaa86f81c8ec23ae3278885
LarissaG3 <109036662+LarissaG3@users.noreply.github.com>
2023-10-24 20:08:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-const listaDeBotoes = document.querySelector('.tecla_pom');
-listaDeBotoes[0].onclick = tocaSomPom;
+const listaDeBotoes = document.querySelector('.tecla_pom').onclick= tocaSomPom
+
+let contador = 0
+
+ while (contador <9) {
+    listaDeBotoes[contador] .onclick = tocaSomPom
+    contador = contador + 1;
+ }
+ 
